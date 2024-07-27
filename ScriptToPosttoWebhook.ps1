2303458e125e7bd2e0340cf224ea57709d7a7b3a
Subba Reddi Tummuru <69682598+SubbaReddi@users.noreply.github.com>
2024-07-27 09:37:39 -0600

    Add files via upload

---
+++ ScriptToPosttoWebhook.ps1
@@ -0,0 +1,28 @@
+﻿$headers = @{
+    'Content-Type' = 'application/json'
+}
+
+$body = @{
+    "type" = "message"
+    "attachments" = @(
+        @{
+            "contentType" = "application/vnd.microsoft.card.adaptive"
+            "contentUrl" = $null
+            "content" = @{
+                "$schema" = "http://adaptivecards.io/schemas/adaptive-card.json"
+                "type" = "AdaptiveCard"
+                "version" = "1.2"
+                "body" = @(
+                    @{
+                        "type" = "TextBlock"
+                        "text" = "For Samples and Templates, see [https://adaptivecards.io/samples](https://adaptivecards.io/samples)"
+                    }
+                )
+            }
+        }
+    )
+}
+
+$jsonBody = $body | ConvertTo-Json -Depth 10
+
+Invoke-RestMethod -Uri "ReplaceWithWebhookUrl" -Method Post -Headers $headers -Body $jsonBody
